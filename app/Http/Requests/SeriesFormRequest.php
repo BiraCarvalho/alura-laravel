921ab22dc3f9c0785f02215f85f53aebac0caf9e
Ubiratã <bira@biracarvalho.com.br>
2019-12-30 18:04:50 -0300

    P01A06

---
+++ app/Http/Requests/SeriesFormRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SeriesFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nome' => 'required|min:3'
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.min' => 'O campo nome precisa ter pelo menos três caracteres'
+        ];
+    }
+}
